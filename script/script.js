b8fd2abd681cee51e2c9866a570203830d945bc7
Benny-zh <61690593+Benny-zh@users.noreply.github.com>
2023-07-27 18:25:31 +0200

    Replace

--- script/script.js
+++ script/script.js
@@ -24,18 +24,35 @@ $(document).ready(function(){
      });
 
      // typing animation script
-     var typed = new Typed(".typing", {
-          strings: ["Web Developer", "Back-end developer"],
-          typeSpeed: 100,
-          backSpeed: 60,
-          loop: true
-     });
-     var typed = new Typed(".typing-2", {
-          strings: ["Web Developer", "Back-end developer"],
-          typeSpeed: 100,
-          backSpeed: 60,
-          loop: true
-     });
+
+     if($(window).width()<690) {
+          var typed = new Typed(".typing", {
+               strings: ["Web Developer", "Back-end developer"],
+               typeSpeed: 100,
+               backSpeed: 60,
+               loop: false
+          });
+          var typed = new Typed(".typing-2", {
+               strings: ["Web Developer", "Back-end developer"],
+               typeSpeed: 100,
+               backSpeed: 60,
+               loop: false
+          });
+     }
+     else {
+          var typed = new Typed(".typing", {
+               strings: ["Web Developer", "Back-end developer"],
+               typeSpeed: 100,
+               backSpeed: 60,
+               loop: true
+          });
+          var typed = new Typed(".typing-2", {
+               strings: ["Web Developer", "Back-end developer"],
+               typeSpeed: 100,
+               backSpeed: 60,
+               loop: true
+          });
+     }; 
 
      // video popup script
      document.querySelectorAll('.myworks .popup-click').forEach(a => {
